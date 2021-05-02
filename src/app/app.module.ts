bb85adb20a657e32fadbdf97ce5eebfa14fe273c
maulesoft <vmedel@maulesoft.cl>
2021-05-01 23:45:23 -0400

    primer commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { Heroesmodule } from './heroes/heroes.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+ 
+  ],
+  imports: [
+    BrowserModule,
+    Heroesmodule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/heroes/heroe/heroe.component.ts
@@ -0,0 +1,33 @@
+import { templateJitUrl } from "@angular/compiler";
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector:'app-heroe',
+    templateUrl: 'heroe.component.html'
+
+})
+
+export class HeroeComponent{
+
+    nombre: string = 'Ironman';
+    edad:number = 45
+
+ // este esun getter
+    get nombreCapitalizado():string {
+        return this.nombre.toUpperCase();
+    }
+// esto es un metodo
+    obtenerNombre(): string {
+        return `${this.nombre} - ${this.edad} `;
+
+    }
+
+    cambiarNombre(): void{
+        this.nombre ='Spiderman';
+         
+    }
+    cambiarEdad(): void{
+        this.edad = 50;
+    }
+}
